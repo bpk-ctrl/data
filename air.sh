876280e5d8c74786198c9e50f15694c21a927bfb
bpk-ctrl <benpk19@duck.com>
2024-08-20 12:26:43 +0200

    Add files via upload

---
+++ air.sh
@@ -0,0 +1,1 @@
+echo Welcome to the automated airodump-ng script! && sudo airmon-ng check kill && sudo airmon-ng start wlan0 && iwconfig && sudo airodump-ng -M wlan0
---
+++ air1.sh
@@ -0,0 +1,1 @@
+echo Welcome to the automated airodump-ng script that does not kill the network processes! && sudo airmon-ng start wlan0 && iwconfig && sudo airodump-ng -M wlan0
